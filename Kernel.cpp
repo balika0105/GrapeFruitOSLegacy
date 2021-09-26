277defd9afca78ef29499ebebf8eef34d6a18610
Balázs M <40917873+balika0105@users.noreply.github.com>
2021-09-26 18:55:27 +0200

    Delete Kernel.cpp

--- Kernel.cpp
+++
@@ -1,8 +0,0 @@
-#include "TextPrint.cpp" //Our text printing thingy
-
-//extern "C" is needed for linker script. It doesn't like C++
-extern "C" void _start(){
-    SetCursorPosition(PositionFromCoords(0,0));
-    PrintString("Hello GrapeFruitOS!\n\n\rComing Soon!");
-    return;
-}
